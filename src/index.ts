05a2023fa4c4a163e6ed0944ff7254e252f35e40
tamas <tamas@debian-BULLSEYE-live-builder-AMD64>
2024-04-01 23:35:33 +0200

    initial - builds a lib

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import MyButton from "./components/MyButton.vue";
+import HelloWorldX from "./components/HelloWorldX.vue";
+
+export {
+  MyButton,
+  HelloWorldX,
+}
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // src/indext.ts is where we have exported the component(s)
+      // entry: resolve(__dirname, "src/index.ts"),
+      entry: "src/index.ts",
+      name: "bonolib",
+      // the name of the output files when the build is run
+      fileName: "bonolib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+})
